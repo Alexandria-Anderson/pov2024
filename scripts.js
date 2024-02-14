43f848ea4dcca13943838780dd0583158412bca7
Alexandria Anderson <aleand@ad.unc.edu>
2024-02-14 17:29:18 -0500

    base comit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Your JavaScript code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to run after the DOM has fully loaded
+    console.log('Page loaded!');
+});
