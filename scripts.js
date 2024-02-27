ac1b9dc00ddc7d427a9b98dea8faa118829f6859
Alexandria Anderson <77866068+Alexandria-Anderson@users.noreply.github.com>
2024-02-27 15:09:22 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Your JavaScript code goes here
-document.addEventListener('DOMContentLoaded', function () {
-    // Code to run after the DOM has fully loaded
-    console.log('Page loaded!');
-});
